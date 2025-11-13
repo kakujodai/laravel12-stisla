b91c6d90ac548a7e70133d33fd0f969a2bd4cdba
Emilee Breckenridge <ebreckenridge1@gulls.salisbury.edu>
2025-11-13 16:31:41 -0500

    get rid of not needed geo_features thing

--- app/Models/FileUpload.php
+++ app/Models/FileUpload.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class FileUpload extends Model
 {
-    //Connect geo_features to fileUpload
-    public function features()
-    {
-        return $this->hasMany(GeoFeature::class);
-    }
     // Allow assignment for fields
     protected $fillable = [
         'user_id',
